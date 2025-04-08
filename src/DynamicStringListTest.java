cc052ed8a6868742ce2306f69fe7199ee1a2d17f
Tim <timmywilliams4665@gmail.com>
2025-04-08 16:59:36 -0700

    Added tests for removeMethod and setMethod

--- src/DynamicStringListTest.java
+++ src/DynamicStringListTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DynamicStringListTest
 {
+
     @Test
     void testAddMethod()
     {
@@ -27,5 +28,31 @@ class DynamicStringListTest
 
         assertEquals("Hello", result);
     }
+
+    @Test
+    void testSetMethod()
+    {
+        DynamicStringList list = new DynamicStringList();
+        
+        list.add("Hello");
+        list.add("Good");
+
+        list.set(0, "Goodbye");
+
+        assertEquals("Goodbye", list.get(0));
+    }
+
+    @Test
+    void testRemoveMethod()
+    {
+        DynamicStringList list = new DynamicStringList();
+        
+        list.add("Hello");
+        list.add("Good");
+        assertEquals(2, list.size());
+        list.remove(0);
+        assertEquals(1, list.size());
+    }
+
 
 }
